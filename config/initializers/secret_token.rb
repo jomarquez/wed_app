5a9d9c1c4d6c236603c7aca0b6f39d25a064de03
Joanna Marquez <joanna.marquez@gmail.com>
2011-12-16 10:14:58 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WedApp::Application.config.secret_token = 'f5d094fa2837ad4b9f48292ee1e8adc56f81e1cb2aa767f25592a93ef1c45b7bdf299f94d193f473b41064fe808891a0f5c7644a877cb9e6f6134cfd7ebed443'
